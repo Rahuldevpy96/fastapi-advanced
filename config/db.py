d60c257843d6c929eb97ed74431011ff3c9a0d69
Rahuldevpy96 <rahuldevpy@gmail.com>
2024-01-17 18:56:26 +0530

    changed the db

--- config/db.py
+++ config/db.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
-# MONGO_URI="mongodb+srv://kneha5489:rdslJF2rCMp8PqCM@cluster0.dwxshzd.mongodb.net"
-MONGO_URI="mongodb+srv://kneha5489:rdslJF2rCMp8PqCM@cluster0.dwxshzd.mongodb.net"
+MONGO_URI="mongodb://localhost:27017"
 
 conn=MongoClient(MONGO_URI)
 
