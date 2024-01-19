2a12574a4373b02d50a0b0eaca616e6d003ba815
Rahuldevpy96 <rahuldevpy@gmail.com>
2024-01-19 14:42:06 +0530

    from use

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from routers.note import note
 from fastapi.staticfiles import StaticFiles
 from routers.user import user
 from routers.todo import todo
+from routers.form import form
 
 
 app=FastAPI()
 app.include_router(note,tags=['Notes'])
 app.include_router(user,tags=['Users'])
 app.include_router(todo,tags=['Todo'])
+app.include_router(form,tags=['Language'])
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
