fa736ede04f78c6c867b2eb590d929996d511f81
Rahuldevpy96 <rahuldevpy@gmail.com>
2024-01-19 16:22:52 +0530

    names correct

--- routers/todo.py
+++ routers/todo.py
@@ -6,20 +6,21 @@ from schemas.todo import todo_serializer,todos_serializer
 
 todo=APIRouter()
 
-@todo.get('/get')
+@todo.get('/todo')
 def get_todo():
+    '''Docstring'''
     todo=todos_serializer(conn.Notes.todo.find())
     return {"status":"ok","data":todo}
 
 
-@todo.post('/post')
-def get_todo(todo:Todo):
+@todo.post('/todo')
+def post_todo(todo:Todo):
     todos=conn.Notes.todo.insert_one(dict(todo))
     todo=todos_serializer(conn.Notes.todo.find({"_id":todos.inserted_id}))
     return ({"Stutus":"Success","data":todo})
 
 
-@todo.put('/update/{id}')
+@todo.put('/todo/{id}')
 def update_todo(id,todo:Todo):
     todos=conn.Notes.todo.find_one_and_update({"_id":(ObjectId(id))},{
         "$set":dict(todo)
@@ -27,7 +28,7 @@ def update_todo(id,todo:Todo):
     todo=todos_serializer(conn.Notes.todo.find({"_id":ObjectId(id)}))
     return ({"Stutus":"Success","data":todo})
 
-@todo.delete('/delete/{id}')
-def update_todo(id,todo:Todo):
+@todo.delete('/todo/{id}')
+def delete_todo(id,todo:Todo):
     conn.Notes.todo.find_one_and_delete({"_id":(ObjectId(id))})
     return ({"Stutus":"Success"})
