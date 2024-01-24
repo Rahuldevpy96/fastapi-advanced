be46e59c8070b8aac275d81a9eaeafa275e4afdf
Rahuldevpy96 <rahul.dataclaps@gmail.com>
2024-01-24 17:55:36 +0530

    email password validation done

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, constr
 
 
 class User(BaseModel):
     name:str
-    email:str
-    password:str
+    email:EmailStr
+    password:constr(min_length=8, max_length=50)
