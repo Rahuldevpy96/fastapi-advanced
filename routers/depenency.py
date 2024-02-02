275f659b33005cbf07216b265c59b37443b8f9e5
Rahuldevpy96 <rahul.dataclaps@gmail.com>
2024-02-02 21:07:30 +0530

    use dependency injection

--- routers/depenency.py
+++ routers/depenency.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 depen=APIRouter()
 
 
-@depen.get('/items/')
-async def read_items(q:str | None=None, skip:int=0, limit:int=100):
+async def common_parameteres(q:str | None=None, skip:int=0, limit:int=100):
     return {"q":q,"skip":skip,"limit":limit}
+
+
+@depen.get('/items/')
+async def read_items(common:dict=Depends(common_parameteres)):
+    return common
+
+
+@depen.get('/users/')
+async def read_users(common:dict=Depends(common_parameteres)):
+    return common
