01608e0a34a201186a4939d466f7332d384ff8f9
Rahuldevpy96 <rahul.dataclaps@gmail.com>
2024-02-05 18:44:54 +0530

    second commit

--- routers/depenency.py
+++ routers/depenency.py
@@ -4,6 +4,7 @@ depen=APIRouter()
 
 
 async def common_parameteres(q:str | None=None, skip:int=0, limit:int=100):
+    '''By calling this function we will be able to get the args of this function in another function too'''
     return {"q":q,"skip":skip,"limit":limit}
 
 
